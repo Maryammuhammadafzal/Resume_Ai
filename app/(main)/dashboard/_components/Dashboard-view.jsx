53ce6d66dcf9dce7dd8b034aab58da056d3dfe0d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-19 01:36:08 +0500

    Add gemini-ai feature

--- app/(main)/dashboard/_components/Dashboard-view.jsx
+++ app/(main)/dashboard/_components/Dashboard-view.jsx
@@ -1,12 +1,48 @@
-"use client"
-import React from 'react'
+"use client";
+import { format, formatDistanceToNow } from "date-fns";
+import { LineChart, TrendingDown, TrendingUp } from "lucide-react";
+import React from "react";
 
 const DashboardView = ({ insights }) => {
-  return (
-    <div>
-      Insights
-    </div>
-  )
-}
+  const salaryData = insights.salaryRanges.map((range) => ({
+    name: range.role,
+    min: range.min / 1000,
+    max: range.max / 1000,
+    median: range.median / 1000,
+  }));
+
+  const getDemandLevelColor = (level) => {
+    switch (level.toLowerCase()) {
+      case "high":
+        return "bg-green-500";
+      case "medium":
+        return "bg-yellow-500";
+      case "low":
+        return "bg-red-500";
+      default:
+        return "bg-gray-500";
+    }
+  };
+
+  const getMarketOutlookInfo = (outlook) => {
+    switch (outlook.toLowerCase()) {
+      case "positive":
+        return { icon: TrendingUp, color: "text-green-500" };
+      case "neutral":
+        return { icon: LineChart, color: "text-yellow-500" };
+      case "positive":
+        return { icon: TrendingDown, color: "text-red-500" };
+      case "positive":
+        return { icon: LineChart, color: "text-gray-500" };
+    }
+  };
+
+  const outlookInfo = getMarketOutlookInfo(insights.marketOutlook).icon;
+  const outlookColor = getMarketOutlookInfo(insights.marketOutlook).color;
+
+  const lastUpdated = format(new Date(insights.lastUpdated), "dd/MM/yyyy");
+  const nextUpdateDistance = formatDistanceToNow(new Date(insights.nextupdate), {addSuffix : true})
+  return <div>Insights</div>;
+};
 
 export default DashboardView;
