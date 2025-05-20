a7d3162b12259519c83f5c4d864aba647d8724b4
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-21 00:19:31 +0500

    adding inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,10 @@
 
-import { inngest } from "@/lib/Inngest/client"
+import { inngest } from "@/lib/inngest/client"
 import { serve } from "inngest/next"
 
-export const { GET , POST , PUT , DELETE } = serve({
-        client : inngest,
-        function : [
-                
+export const { GET, POST, PUT, DELETE } = serve({
+        client: inngest,
+        function: [
+
         ]
 })
