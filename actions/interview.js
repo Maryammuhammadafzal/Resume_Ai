bebb99c37875bbe41023c17e3fcfcc2dd4b8f78f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-09 21:20:43 +0500

    start working to fix bugs

--- actions/interview.js
+++ actions/interview.js
@@ -108,7 +108,7 @@ Don't explicitly mention the mistakes, instead focus on what to learn/practice.
                                 data: {
                                         userId: user.id,
                                         quizScore: score,
-                                        questions: questionResults,
+                                        question: questionResults,
                                         category: "Technical",
                                         improvementTip,
                                 }
