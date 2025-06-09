21430bdc7cbdf4912290035a5cc86b6a8cfa0945
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-10 03:13:18 +0500

    addingfunctions

--- app/(main)/interview/_components/stats-cards.jsx
+++ app/(main)/interview/_components/stats-cards.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const StatsCards = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const StatsCards = ({ assessments }) => {
+  const getAverageScore = () => {
+    if (!assessments?.length) return 0;
+    const total = assessments.reduce(
+      (sum, assessment) => sum + assessment.quizScore,
+      0
+    );
+    return (total / assessments.length).toFixed(1);
+  };
+  return <div className="grid md:grid-cols-3 gap-4"></div>;
+};
 
-export default StatsCards
+export default StatsCards;
